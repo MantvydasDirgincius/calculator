21cf4561663bed855cd57febedeae690489904e4
Mantvydas <mantvydas.dirgincius@icloud.com>
2025-01-28 00:15:10 +0200

    calculation update

--- app/Http/Controllers/CalculationController.php
+++ app/Http/Controllers/CalculationController.php
@@ -23,7 +23,6 @@ class CalculationController
     }
     public function calculate(CalculationRequest $request)
     {
-
         $numbers = $request->numbers;
         $operators = $request->operators;
 
@@ -33,32 +32,53 @@ class CalculationController
             ], 400);
         }
 
-        $result = array_shift($numbers);
-        foreach ($operators as $index => $operator) {
-            $nextNumber = $numbers[$index];
-            $result = match ($operator) {
-                '+' => $result + $nextNumber,
-                '-' => $result - $nextNumber,
-                '*' => $result * $nextNumber,
-                '/' => $nextNumber != 0 ? $result / $nextNumber : 'N/A',
-                default => throw new \InvalidArgumentException('Invalid Operator'),
-            };
+
+        $tempNumbers = [$numbers[0]];
+        $tempOperators = [];
+        for ($i = 0; $i < count($operators); $i++) {
+            if ($operators[$i] === '*' || $operators[$i] === '/') {
+                $prevNumber = array_pop($tempNumbers);
+                $currentNumber = $numbers[$i + 1];
+                $result = $operators[$i] === '*'
+                    ? $prevNumber * $currentNumber
+                    : ($currentNumber != 0 ? $prevNumber / $currentNumber : 'N/A');
+
+                if ($result === 'N/A') {
+                    return response()->json([
+                        'error' => 'Division by zero detected.'
+                    ], 400);
+                }
+                $tempNumbers[] = $result;
+            } else {
+                $tempNumbers[] = $numbers[$i + 1];
+                $tempOperators[] = $operators[$i];
+            }
         }
 
-        \Auth::user()->calculations()->create([
-            // 'user_id' => auth()->id(),
 
+        $result = array_shift($tempNumbers);
+        foreach ($tempOperators as $index => $operator) {
+            $nextNumber = $tempNumbers[$index];
+            $result = $operator === '+'
+                ? $result + $nextNumber
+                : $result - $nextNumber;
+        }
+
+
+        \Auth::user()->calculations()->create([
             'operation' => implode(' ', $operators),
             'numbers' => $request->numbers,
             'result' => $result,
         ]);
+
         $previous_calculations = \Auth::user()->calculations()
             ->latest()
             ->take(10)
             ->get();
+
         $processData = [
             'result' => $result,
-            'previous_calculations' => $previous_calculations
+            'previous_calculations' => $previous_calculations,
         ];
 
         return redirect()->back()->with(["processData" => $processData]);
